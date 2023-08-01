097f003c9a4d1af46bf66bc8fcaff5b373acb92d
Carlos <carlos.w.0713@outlook.com>
2023-08-01 14:42:56 +0800

    初始化操作

--- 算法练习/test.py
+++ 算法练习/test.py
@@ -1,70 +1,20 @@
-class Solution:
-    def divide(self, dividend: int, divisor: int) -> int:
-
-        def div(dividend, divisor):
-
-            if dividend < divisor:
-                return 0
-
-            cur = divisor
-            multiple = 1
-
-            while cur + cur <dividend:  # 用加法求出保证divisor * multiple <= dividend的最大multiple
-                cur += cur  # 即cur分别乘以1, 2, 4, 8, 16...2^n，即二进制搜索
-                multiple += multiple
-
-            return multiple + div(dividend - cur, divisor)
-
-        if (divisor>=0 and dividend>=0) or (divisor<0 and dividend<0):
 
 
-            if divisor < 0 and dividend < 0:
-                divisor = -divisor
-                dividend = -dividend
+class Person:
+    gender='男生'
 
-            res = div(dividend, divisor)
-            return res
+    def __init__(self, name, age):  # 初始化方法
+        self.name = name  # 实例化属性
+        self.age = age  # 实例化属性
 
-        else:
-            if divisor<0:
-                divisor=-divisor
-            else:
-                dividend=-dividend
+    def say_what(self,say_out):  # 类方法
 
-            res = div(dividend, divisor)
-            return -res
+        print(f"{say_out}, my name is {self.name} and I'm {self.age} years old.")
 
+# 创建实例并传入参数
+person1 = Person("John", 25)
+person2 = Person("Alice", 30)
+person_gender=Person.gender
+Person.say_what("你好")
 
-
-def rotate(matrix):
-    n = len(matrix)
-    # 沿着主对角线翻转
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-            print(matrix[0],'\n',matrix[1],'\n',matrix[2])
-    # 每一行沿着中心列翻转
-    for i in range(n):
-        for j in range(n // 2):
-            matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
-            print(matrix)
-
-
-if __name__=='__main__':
-
-    haystack = -2147483647
-    needle = -1
-
-
-    # print(Solution.divide(1,haystack,needle))
-
-
-    left = len(s) - 1
-    right = 0
-    while left > right:
-        List[left], List[right] = List[right], List[left]
-        left -= 1
-        right += 1
-    return s
-
-
+print(person1.name,person2.gender,person_gender)
